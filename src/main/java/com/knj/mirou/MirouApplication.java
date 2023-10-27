204e60d9aaf2e154284aeb4ef2fbc44ce89dcab1
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-10-27 17:07:06 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/knj/mirou/MirouApplication.java
@@ -0,0 +1,13 @@
+package com.knj.mirou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knj/mirou/MirouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knj.mirou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
