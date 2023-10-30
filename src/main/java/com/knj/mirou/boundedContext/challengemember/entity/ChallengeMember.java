f0d74f0a4781fe0e94f5872b5db2bb3235258763
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-10-30 15:52:15 +0900

    Refactor: ChallengeMember 엔티티 재설계

---
+++ src/main/java/com/knj/mirou/boundedContext/challengemember/entity/ChallengeMember.java
@@ -0,0 +1,31 @@
+package com.knj.mirou.boundedContext.challengemember.entity;
+
+import com.knj.mirou.base.entity.BaseEntity;
+import com.knj.mirou.boundedContext.challenge.entity.Challenge;
+import com.knj.mirou.boundedContext.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+@ToString(callSuper = true)
+public class ChallengeMember extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Challenge linkedChallenge;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member linkedMember;
+
+    private int successNumber;
+}
