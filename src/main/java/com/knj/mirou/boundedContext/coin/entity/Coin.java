43eb3e8a30aa0cfc5bb92769278c5bea943dd561
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-10-30 15:54:30 +0900

    Refactor: Coin 엔티티 재설계

---
+++ src/main/java/com/knj/mirou/boundedContext/coin/entity/Coin.java
@@ -0,0 +1,33 @@
+package com.knj.mirou.boundedContext.coin.entity;
+
+import com.knj.mirou.base.entity.BaseEntity;
+import com.knj.mirou.boundedContext.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+@ToString(callSuper = true)
+public class Coin extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member linkedMember;
+
+    private int currentCoin;
+
+    private int totalGetCoin;
+
+    private int totalUserCoin;
+}
