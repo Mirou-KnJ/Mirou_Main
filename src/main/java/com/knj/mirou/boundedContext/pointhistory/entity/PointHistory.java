aef5cb5acc429d7bcd2d7ad85599a2bbfc103bb5
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-10-30 16:09:52 +0900

    Refactor: PointHistory 엔티티 수정

---
+++ src/main/java/com/knj/mirou/boundedContext/pointhistory/entity/PointHistory.java
@@ -0,0 +1,32 @@
+package com.knj.mirou.boundedContext.pointhistory.entity;
+
+import com.knj.mirou.base.entity.BaseEntity;
+import com.knj.mirou.boundedContext.member.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+@ToString(callSuper = true)
+public class PointHistory extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member linkedMember;
+
+    private int changePoint;
+
+    private String PointContents;
+
+}
