863c87356a27113a2f2cbf54fa0a3c673e82fa03
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-10-30 16:03:40 +0900

    Refactor: Member엔티티 수정

--- src/main/java/com/knj/mirou/boundedContext/member/Member.java
+++ src/main/java/com/knj/mirou/boundedContext/member/Member.java
@@ -1,31 +1,38 @@
 package com.knj.mirou.boundedContext.member;
 
+import com.knj.mirou.base.entity.BaseEntity;
 import com.knj.mirou.boundedContext.coin.entity.Coin;
 import com.knj.mirou.boundedContext.point.Point;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 import java.util.Date;
 
+@Getter
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class Member {
+@SuperBuilder(toBuilder = true)
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
 
-    @Id
-    private long id;
-    private String loginId; //로그인 아이디
-    private String nickname; //닉네임
-    private String socialCode; //로그인 경로
-    private String role; //권한
-    private String inviteCode; //초대코드
-    private Date createdAt; //생성된 시간
+    private String loginId;
+
+    private String nickname;
+
+    private String socialCode;
+
+    private MemberRole role;
+
+    private String inviteCode;
 
     @OneToOne(mappedBy = "owner")
     private Coin coin;
 
-    //프로필 이미지
 }
