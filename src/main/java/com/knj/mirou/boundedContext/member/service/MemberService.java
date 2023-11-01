38ce2dcbc00320013adacec96ac71c0ff1ba3734
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-01 11:15:29 +0900

    Feat: findByLoginId 메서드

--- src/main/java/com/knj/mirou/boundedContext/member/service/MemberService.java
+++ src/main/java/com/knj/mirou/boundedContext/member/service/MemberService.java
@@ -1,9 +1,12 @@
 package com.knj.mirou.boundedContext.member.service;
 
+import com.knj.mirou.boundedContext.member.model.entity.Member;
 import com.knj.mirou.boundedContext.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    public Optional<Member> getByLoginId(String loginId) {
+
+        return memberRepository.findByLoginId(loginId);
+    }
 
 }
