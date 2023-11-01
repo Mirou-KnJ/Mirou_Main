8fa1e4848ec247ce2b59f292040ae0b335f49818
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-01 11:30:13 +0900

    Feat: 회원가입시 코인은 0으로 제공

--- src/main/java/com/knj/mirou/boundedContext/member/service/MemberService.java
+++ src/main/java/com/knj/mirou/boundedContext/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.knj.mirou.boundedContext.member.service;
 
+import com.knj.mirou.boundedContext.coin.service.CoinService;
 import com.knj.mirou.boundedContext.member.model.entity.Member;
 import com.knj.mirou.boundedContext.member.repository.MemberRepository;
 import com.knj.mirou.boundedContext.point.service.PointService;
@@ -18,12 +19,14 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PointService pointService;
+    private final CoinService coinService;
 
     public Optional<Member> getByLoginId(String loginId) {
 
         return memberRepository.findByLoginId(loginId);
     }
 
+    @Transactional
     public Map<String, String> join(String socialCode, String loginId, String password) {
 
         Map<String, String> joinResultMap = new HashMap<>();
@@ -35,6 +38,7 @@ public class MemberService {
 
             Member member = Member.builder()
                     .point(pointService.createPoint())
+                    .coin(coinService.createCoin())
                     .build();
 
 
