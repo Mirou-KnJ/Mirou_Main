6c0ee174aa952ef13f8888da27012983a722e22c
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-03 11:14:51 +0900

    Feat: 마이페이지 로그인 여부 체크, 멤버 데이터 모델에 삽입

--- src/main/java/com/knj/mirou/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/knj/mirou/boundedContext/member/controller/MemberController.java
@@ -1,10 +1,17 @@
 package com.knj.mirou.boundedContext.member.controller;
 
+import com.knj.mirou.boundedContext.member.model.entity.Member;
 import com.knj.mirou.boundedContext.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,8 +25,21 @@ public class MemberController {
         return "/view/member/login";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/myPage")
-    public String showMyPage(){
+    public String showMyPage(Principal principal, Model model){
+
+        String loginId = principal.getName();
+
+        Optional<Member> ObyLoginId = memberService.getByLoginId(loginId);
+
+        if(ObyLoginId.isPresent()) {
+            model.addAttribute("member", ObyLoginId.get());
+        } else {
+            //FIXME 에러페이지 렌더링?
+            return null;
+        }
+
         return "/view/member/mypage";
     }
 
