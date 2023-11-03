02cd212d8e790bc802b2dc905a669b9eb3ce4c9b
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-03 10:21:13 +0900

    Refactor: (login, mypage) fix href, controller mapping

--- src/main/java/com/knj/mirou/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/knj/mirou/boundedContext/member/controller/MemberController.java
@@ -2,12 +2,25 @@ package com.knj.mirou.boundedContext.member.controller;
 
 import com.knj.mirou.boundedContext.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
+
+    @GetMapping("/login")
+    public String showLoginPage(){
+        return "/view/member/login";
+    }
+
+    @GetMapping("/myPage")
+    public String showMyPage(){
+        return "/view/member/mypage";
+    }
 
 }
