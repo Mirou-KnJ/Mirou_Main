11653bf6c2200ada8af3dc97e4abc73b75b41953
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-06 14:30:25 +0900

    Feat: 인증하기 페이지 매핑 및 이동가능  로그인한 회원은 인증하기 버튼을 눌러 인증하러 이동 가능

---
+++ src/main/java/com/knj/mirou/boundedContext/challengefeed/controller/ChallengeFeedRestController.java
@@ -0,0 +1,14 @@
+package com.knj.mirou.boundedContext.challengefeed.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/feed")
+public class ChallengeFeedRestController {
+
+
+}
