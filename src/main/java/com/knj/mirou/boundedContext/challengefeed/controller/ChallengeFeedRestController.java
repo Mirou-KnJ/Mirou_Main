41452c8347b3fedd2099b5d280dffd1492c68261
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-06 14:57:00 +0900

    Feat: 파일 폼 전송 확인

--- src/main/java/com/knj/mirou/boundedContext/challengefeed/controller/ChallengeFeedRestController.java
+++ src/main/java/com/knj/mirou/boundedContext/challengefeed/controller/ChallengeFeedRestController.java
@@ -1,14 +1,23 @@
 package com.knj.mirou.boundedContext.challengefeed.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/feed")
+@RequestMapping("/api/feed")
 public class ChallengeFeedRestController {
+
+    @PostMapping("/write")
+    public void writeRequest(MultipartFile img) {
+
+        System.out.println(img.getName());
+        System.out.println("img = " + img.getOriginalFilename());
+
+    }
 
 
 }
