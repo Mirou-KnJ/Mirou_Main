9f919008ae9c933fe24c557dcdea5612dbd1c264
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-06 15:36:13 +0900

    Feat: AmazonS3 Client Bean 생성 yml파일의 내용을 토대로 S3클라이언트 생성

---
+++ src/main/java/com/knj/mirou/boundedContext/image/service/ImageService.java
@@ -0,0 +1,10 @@
+package com.knj.mirou.boundedContext.image.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class ImageService {
+
+}
