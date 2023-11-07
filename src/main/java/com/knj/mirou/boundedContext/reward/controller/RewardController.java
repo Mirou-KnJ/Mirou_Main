30daf72983cb0a4e6ae2fd0db0ab524e060778c2
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-07 12:54:23 +0900

    Feat: 챌린지 보상 설정 임시 프론트 구현

--- src/main/java/com/knj/mirou/boundedContext/reward/controller/RewardController.java
+++ src/main/java/com/knj/mirou/boundedContext/reward/controller/RewardController.java
@@ -3,10 +3,11 @@ package com.knj.mirou.boundedContext.reward.controller;
 import com.knj.mirou.boundedContext.reward.service.PublicRewardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 
 @Controller
@@ -17,12 +18,19 @@ public class RewardController {
     private final PublicRewardService publicRewardService;
 
     @GetMapping("/setting/{challengeId}")
-    public String settingForm(@PathVariable(value = "challengeId") long challengeId) {
+    public String settingForm(@PathVariable(value = "challengeId") long challengeId, Model model) {
 
+        model.addAttribute("challengeId", challengeId);
 
-        return "/view/reward/settingForm" +challengeId;
+        return "/view/reward/settingForm";
     }
 
+    @PostMapping("/setting/{challengeId}")
+    public String setPublicReward(@PathVariable(value = "challengeId") long challengeId) {
+
+
+        return "";
+    }
 
 
 }
