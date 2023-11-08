c9afa2df25a79b48d57eb092b7891951aba1b57c
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-08 16:10:06 +0900

    Feat: 인증하여 ChallengeFeed 객체 생성 가능

--- src/main/java/com/knj/mirou/boundedContext/challengefeed/controller/ChallengeFeedController.java
+++ src/main/java/com/knj/mirou/boundedContext/challengefeed/controller/ChallengeFeedController.java
@@ -1,19 +1,43 @@
 package com.knj.mirou.boundedContext.challengefeed.controller;
 
+import com.knj.mirou.boundedContext.challengefeed.service.ChallengeFeedService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/feed")
 public class ChallengeFeedController {
 
-    @GetMapping("/writeForm")
-    public String writeFeed() {
+    private final ChallengeFeedService challengeFeedService;
 
-        return "/view/challengefeed/writeForm";
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/write/{id}")
+    public String writeForm(@PathVariable(value = "id") long challengeId, Model model) {
+
+        model.addAttribute("challengeId", challengeId);
+
+        return "/view/challengeFeed/writeForm";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/write/{id}")
+    public String write(@PathVariable(value = "id") long challengeId, MultipartFile img,
+                        Principal principal) throws IOException {
+
+        challengeFeedService.writeFeed(challengeId, principal.getName(), img);
+
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/knj/mirou/boundedContext/challengefeed/service/ChallengeFeedService.java
+++ src/main/java/com/knj/mirou/boundedContext/challengefeed/service/ChallengeFeedService.java
@@ -1,6 +1,12 @@
 package com.knj.mirou.boundedContext.challengefeed.service;
 
+import com.knj.mirou.boundedContext.challenge.model.entity.Challenge;
+import com.knj.mirou.boundedContext.challenge.service.ChallengeService;
+import com.knj.mirou.boundedContext.challengefeed.entity.ChallengeFeed;
+import com.knj.mirou.boundedContext.challengefeed.repository.ChallengeFeedRepository;
 import com.knj.mirou.boundedContext.image.service.ImageService;
+import com.knj.mirou.boundedContext.member.model.entity.Member;
+import com.knj.mirou.boundedContext.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,9 +19,25 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class ChallengeFeedService {
 
+    private final ChallengeFeedRepository challengeFeedRepository;
     private final ImageService imageService;
+    private final ChallengeService challengeService;
+    private final MemberService memberService;
 
-    public String uploadImg(MultipartFile img) throws IOException {
+    @Transactional
+    public String writeFeed(long linkedChallengeId, String loginId, MultipartFile img) throws IOException {
+
+        Member writer = memberService.getByLoginId(loginId).get();
+        Challenge linkedChallenge = challengeService.getById(linkedChallengeId);
+
+        String imgUrl = imageService.uploadImg(img);
+
+        ChallengeFeed newFeed = ChallengeFeed.builder()
+                .writer(writer)
+                .linkedChallenge(linkedChallenge)
+                .build();
+
+        challengeFeedRepository.save(newFeed);
 
         return imageService.uploadImg(img);
     }
