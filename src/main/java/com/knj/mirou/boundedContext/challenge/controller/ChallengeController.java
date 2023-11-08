935c9841106f69d32795034e5c4e1d5fbb4d7224
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-08 15:28:59 +0900

    Feat: 참가 여부에 따라 인증하기 / 참가하기 다르게 출력

--- src/main/java/com/knj/mirou/boundedContext/challenge/controller/ChallengeController.java
+++ src/main/java/com/knj/mirou/boundedContext/challenge/controller/ChallengeController.java
@@ -2,7 +2,12 @@ package com.knj.mirou.boundedContext.challenge.controller;
 
 import com.knj.mirou.boundedContext.challenge.model.entity.Challenge;
 import com.knj.mirou.boundedContext.challenge.service.ChallengeService;
+import com.knj.mirou.boundedContext.challengemember.model.entity.ChallengeMember;
+import com.knj.mirou.boundedContext.challengemember.service.ChallengeMemberService;
+import com.knj.mirou.boundedContext.member.model.entity.Member;
+import com.knj.mirou.boundedContext.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +15,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,6 +25,8 @@ import java.time.LocalDate;
 public class ChallengeController {
 
     private final ChallengeService challengeService;
+    private final MemberService memberService;
+    private final ChallengeMemberService challengeMemberService;
 
     @GetMapping("/create")
     public String createChallenge() {
@@ -40,8 +49,20 @@ public class ChallengeController {
         return "view/challenge/allChallengeList";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/detail/{id}")
-    public String detailChallenge(@PathVariable(value = "id") long challengeId, Model model) {
+    public String detailChallenge(@PathVariable(value = "id") long challengeId, Principal principal, Model model) {
+
+        Member loginedMember = memberService.getByLoginId(principal.getName()).get();
+
+        //FIXME : 원래 ChallengeMember 서비스는 여기서 호출하면 안되며, 서비스 처리도 컨트롤러에서 이루어지면 안좋음.
+        Optional<ChallengeMember> byMember = challengeMemberService.getByMember(loginedMember);
+
+        if(byMember.isPresent()) {
+            model.addAttribute("isJoin", true);
+        } else {
+            model.addAttribute("isJoin", false);
+        }
 
         Challenge challenge = challengeService.getById(challengeId);
 
--- src/main/java/com/knj/mirou/boundedContext/challengemember/service/ChallengeMemberService.java
+++ src/main/java/com/knj/mirou/boundedContext/challengemember/service/ChallengeMemberService.java
@@ -1,5 +1,6 @@
 package com.knj.mirou.boundedContext.challengemember.service;
 
+import com.knj.mirou.boundedContext.challengemember.model.entity.ChallengeMember;
 import com.knj.mirou.boundedContext.challengemember.model.enums.Progress;
 import com.knj.mirou.boundedContext.challengemember.repository.ChallengeMemberRepository;
 import com.knj.mirou.boundedContext.member.model.entity.Member;
@@ -7,12 +8,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ChallengeMemberService {
 
     private final ChallengeMemberRepository challengeMemberRepository;
+
+    public Optional<ChallengeMember> getByMember(Member member) {
+
+        return challengeMemberRepository.findByLinkedMember(member);
+    }
 
     public long getCountByMemberAndProgress(Member member, Progress progress) {
 
