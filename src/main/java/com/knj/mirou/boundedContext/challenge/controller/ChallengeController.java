880525b5fd8a8c38126e7bb63034369f5d92cb62
jungwoo4503 <108951535+jungwoo4503@users.noreply.github.com>
2023-11-08 11:10:46 +0900

    챌린지 생성 기능(데이터베이스에 저장) (#32)
    Co-authored-by: jeinst <jeinst77@gmail.com>

---
+++ src/main/java/com/knj/mirou/boundedContext/challenge/controller/ChallengeController.java
@@ -0,0 +1,52 @@
+package com.knj.mirou.boundedContext.challenge.controller;
+
+import com.knj.mirou.boundedContext.challenge.model.entity.Challenge;
+import com.knj.mirou.boundedContext.challenge.service.ChallengeService;
+import com.knj.mirou.boundedContext.coin.entity.Coin;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/challenge")
+public class ChallengeController {
+
+    private final ChallengeService challengeService;
+
+    @GetMapping("/create")
+    public String createChallenge(){
+
+        return "view/challenge/createChallenge";
+    }
+
+    @PostMapping("/create")
+    public String createChallenge(String name, String contents, int joinCost, LocalDate joinDeadLine,
+                                  String period, String tag, String method, int level, String status, String precaution) {
+
+        challengeService.create(name, contents, joinCost, joinDeadLine,
+                                period, tag, method, level, status, precaution);
+
+        return "redirect:/";
+    }
+    @GetMapping("/allChallengeList")
+    public String allChallengeList(Model model){
+        model.addAttribute("challenges", challengeService.getAllChallenges());
+        return "view/challenge/allChallengeList";
+    }
+
+    @GetMapping("/detail")
+    public String detailChallenge(){
+        return "view/challenge/detailChallenge";
+    }
+
+}
---
+++ src/main/java/com/knj/mirou/boundedContext/challenge/service/ChallengeService.java
@@ -0,0 +1,48 @@
+package com.knj.mirou.boundedContext.challenge.service;
+
+import com.knj.mirou.boundedContext.challenge.model.entity.Challenge;
+import com.knj.mirou.boundedContext.challenge.model.enums.AuthenticationMethod;
+import com.knj.mirou.boundedContext.challenge.model.enums.ChallengePeriod;
+import com.knj.mirou.boundedContext.challenge.model.enums.ChallengeStatus;
+import com.knj.mirou.boundedContext.challenge.model.enums.ChallengeTag;
+import com.knj.mirou.boundedContext.challenge.repository.ChallengeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ChallengeService {
+
+    private final ChallengeRepository challengeRepository;
+
+    @Transactional
+    public Challenge create(String name, String contents, int joinCost, LocalDate joinDeadLine,
+                                     String period, String tag, String method, int level, String status, String precautions) {
+
+        Challenge newChallenge = Challenge.builder()
+                .name(name)
+                .contents(contents)
+                .joinCost(joinCost)
+                .joinDeadline(joinDeadLine)
+                .period(ChallengePeriod.valueOf(period))
+                .tag(ChallengeTag.valueOf(tag))
+                .method(AuthenticationMethod.valueOf(method))
+                .level(level)
+                .status(ChallengeStatus.valueOf(status))
+                .precautions(precautions)
+                .build();
+
+        return challengeRepository.save(newChallenge);
+    }
+
+    public List<Challenge> getAllChallenges() {
+        return challengeRepository.findAll();
+    }
+}
