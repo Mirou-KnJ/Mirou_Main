834a424d440ccc5d4ea5d99e1394ba1f7114d495
SeoMoonk <39723465+SeoMoonk@users.noreply.github.com>
2023-11-10 13:12:37 +0900

    챌린지 이미지 추가 및 디테일 페이지 출력, 챌린지 생성 페이지 프론트 개선 (#49)

--- src/main/java/com/knj/mirou/boundedContext/challenge/controller/ChallengeController.java
+++ src/main/java/com/knj/mirou/boundedContext/challenge/controller/ChallengeController.java
@@ -1,10 +1,14 @@
 package com.knj.mirou.boundedContext.challenge.controller;
 
+import com.knj.mirou.base.rsData.RsData;
 import com.knj.mirou.boundedContext.challenge.model.entity.Challenge;
 import com.knj.mirou.boundedContext.challenge.service.ChallengeService;
 import com.knj.mirou.boundedContext.challengefeed.entity.ChallengeFeed;
 import com.knj.mirou.boundedContext.challengefeed.service.ChallengeFeedService;
 import com.knj.mirou.boundedContext.challengemember.service.ChallengeMemberService;
+import com.knj.mirou.boundedContext.imageData.model.entity.ImageData;
+import com.knj.mirou.boundedContext.imageData.model.enums.ImageTarget;
+import com.knj.mirou.boundedContext.imageData.service.ImageDataService;
 import com.knj.mirou.boundedContext.member.model.entity.Member;
 import com.knj.mirou.boundedContext.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +19,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.time.LocalDate;
 import java.util.List;
@@ -29,20 +35,39 @@ public class ChallengeController {
     private final MemberService memberService;
     private final ChallengeMemberService challengeMemberService;
     private final ChallengeFeedService challengeFeedService;
+    private final ImageDataService imageDataService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/create")
-    public String createChallenge() {
+    public String createForm() {
 
-        return "view/challenge/createChallenge";
+        return "/view/challenge/createForm";
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/create")
-    public String createChallenge(String name, String contents, int joinCost, LocalDate joinDeadLine, int period,
-                                  String tag, String method, int level, String precaution) {
+    public String createChallenge(String name, String contents, MultipartFile img, int joinCost, LocalDate joinDeadLine,
+                                  int period, String tag, String method, int level, String precaution) throws IOException {
 
-        challengeService.create(name, contents, joinCost, joinDeadLine, period, tag, method, level, precaution);
+        RsData<Challenge> createRsData =
+                challengeService.create(name, contents, joinCost, joinDeadLine, period, tag, method, level, precaution);
 
-        return "redirect:/";
+        if (createRsData.isFail()) {
+            //TODO : 오류 원인 반환 후 다시 생성페이지로 리다이렉트
+            return "redirect:/challenge/create";
+        }
+
+        long challengeId = createRsData.getData().getId();
+        RsData<String> uploadRsData = imageDataService.uploadImg(img, ImageTarget.CHALLENGE_IMG);
+
+        if (uploadRsData.isFail()) {
+            //TODO : 오류 원인 반환 후 다시 생성페이지로 리다이렉트
+            return "redirect:/challenge/create";
+        }
+
+        imageDataService.create(challengeId, ImageTarget.CHALLENGE_IMG, uploadRsData.getData());
+
+        return "redirect:/challenge/detail/" + challengeId;
     }
 
     @GetMapping("/allChallengeList")
@@ -58,12 +83,19 @@ public class ChallengeController {
         Member loginedMember = memberService.getByLoginId(principal.getName()).get();
         Challenge challenge = challengeService.getById(challengeId);
         List<ChallengeFeed> feedList = challengeFeedService.getByChallenge(challenge);
+        ImageData challengeImg = imageDataService.getByIdAndTarget(challenge.getId(), ImageTarget.CHALLENGE_IMG);
 
-        //FIXME: challengeMemberService 여기서 호출 X? => 순환 참조 문제 어떻게 할 것인가.
-        if(challengeMemberService.getByChallengeAndMember(challenge, loginedMember).isPresent()) {
+        //TODO: 구조 개선
+        if (challengeMemberService.getByChallengeAndMember(challenge, loginedMember).isPresent()) {
             model.addAttribute("isJoin", true);
         } else {
             model.addAttribute("isJoin", false);
+        }
+
+        if(challengeImg != null) {
+            model.addAttribute("challengeImg", challengeImg.getImageUrl());
+        } else {
+            model.addAttribute("challengeImg", null);
         }
 
         model.addAttribute("challenge", challenge);
