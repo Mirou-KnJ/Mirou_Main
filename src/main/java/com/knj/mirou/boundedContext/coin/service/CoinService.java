bb4d1fd60d3a9fe1d35df1a6decd691bce95258b
jeinst <jeinst77@gmail.com>
2023-11-15 16:47:51 +0900

    Fix: 코인 확률 조정 알고리즘 수정

--- src/main/java/com/knj/mirou/boundedContext/coin/service/CoinService.java
+++ src/main/java/com/knj/mirou/boundedContext/coin/service/CoinService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Random;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -33,17 +35,7 @@ public class CoinService {
         int rewardCoin = Integer.parseInt(reward.getReward());
 
         //TODO: 확률 조정 알고리즘
-        double randomCoin = Math.random();
-
-        if (randomCoin < 0.25) {
-            rewardCoin = (int) (rewardCoin * 0.4);  // 0 ~ 1/4 구간: 40% 확률
-        } else if (randomCoin < 0.5) {
-            rewardCoin = (int)(rewardCoin * 0.3);   // 1/4 ~ 1/2 : 30% 확률
-        } else if (randomCoin < 0.75) {
-            rewardCoin = (int)(rewardCoin * 0.2);   // 1/2 ~ 3/4 : 20% 확률
-        } else {
-            rewardCoin = (int)(rewardCoin * 0.1);   // 3/4 ~ 100 : 10% 확률
-        }
+        int randomCoin = randomCoin(rewardCoin);
 
         //TODO: 지급 히스토리 기록
 
@@ -54,6 +46,22 @@ public class CoinService {
                 .build();
 
         coinRepository.save(coin);
+    }
+
+    private int randomCoin(int rewardCoin) {
+        Random random = new Random();
+        int quarter = rewardCoin / 4;
+        int rand = random.nextInt(rewardCoin);
+
+        if (rand < quarter) {
+            return (int)(rewardCoin * 0.4);
+        } else if (rand < 2 * quarter) {
+            return (int)(rewardCoin * 0.3);
+        } else if (rand < 3 * quarter) {
+            return (int)(rewardCoin * 0.2);
+        } else {
+            return (int)(rewardCoin * 0.1);
+        }
     }
 
 }
