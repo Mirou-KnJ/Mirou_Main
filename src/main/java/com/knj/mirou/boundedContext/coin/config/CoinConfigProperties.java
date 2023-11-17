7333553ca3a079a5c87e3b1016fee1d0a31bf7ec
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-17 16:23:43 +0900

    Feat: 랜덤 지급 확률 설정파일화 (하드코딩 삭제)

---
+++ src/main/java/com/knj/mirou/boundedContext/coin/config/CoinConfigProperties.java
@@ -0,0 +1,19 @@
+package com.knj.mirou.boundedContext.coin.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "custom.coin")
+public class CoinConfigProperties {
+
+    private double quarter;
+    private List<Double> standard;
+
+}
