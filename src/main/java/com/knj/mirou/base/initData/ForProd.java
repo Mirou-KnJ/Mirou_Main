bddc83dea19aa8bee642c04109e9995e056ac179
jeinst <jeinst77@gmail.com>
2023-11-22 11:39:21 +0900

    Remove: import 최적화

--- src/main/java/com/knj/mirou/base/initData/ForProd.java
+++ src/main/java/com/knj/mirou/base/initData/ForProd.java
@@ -1,7 +1,6 @@
 package com.knj.mirou.base.initData;
 
 import com.knj.mirou.boundedContext.challenge.config.LabelConfig;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/knj/mirou/boundedContext/challenge/repository/ChallengeRepository.java
+++ src/main/java/com/knj/mirou/boundedContext/challenge/repository/ChallengeRepository.java
@@ -2,14 +2,9 @@ package com.knj.mirou.boundedContext.challenge.repository;
 
 import com.knj.mirou.boundedContext.challenge.model.entity.Challenge;
 import com.knj.mirou.boundedContext.challenge.model.enums.ChallengeStatus;
-import com.knj.mirou.boundedContext.challengefeed.entity.ChallengeFeed;
-import com.knj.mirou.boundedContext.member.model.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
