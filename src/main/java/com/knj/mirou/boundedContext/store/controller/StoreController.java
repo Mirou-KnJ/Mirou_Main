a3cf9a0bfd8e3b3258feb76539c5cdde445620f8
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-23 13:34:25 +0900

    Feat: store MVC 구조 정의

---
+++ src/main/java/com/knj/mirou/boundedContext/store/controller/StoreController.java
@@ -0,0 +1,13 @@
+package com.knj.mirou.boundedContext.store.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/store")
+@RequiredArgsConstructor
+public class StoreController {
+}
