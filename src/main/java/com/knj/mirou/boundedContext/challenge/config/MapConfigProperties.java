a11bc28a5c74c8d14f271ac48a8ed39574270d61
SeoMoonk <39723465+SeoMoonk@users.noreply.github.com>
2023-11-27 17:16:19 +0900

    Feat: 지도 api 테스트 준비 (#109)
    * Feat: 지도 api 테스트 준비
    
    * Feat: 임시 지도 띄우기
    
    * Chore: keystore.p12 파일 추가
    
    * Fix: fix some - 테스트 배포 시도
    
    * Fix: 오타 수정

---
+++ src/main/java/com/knj/mirou/boundedContext/challenge/config/MapConfigProperties.java
@@ -0,0 +1,16 @@
+package com.knj.mirou.boundedContext.challenge.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "custom.challenge.map")
+public class MapConfigProperties {
+
+    public String key;
+
+}
