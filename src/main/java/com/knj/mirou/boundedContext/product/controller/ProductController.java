74e367fbd248b8c8078524052c92e9925f5bcf67
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-29 16:49:10 +0900

    Feat: 상품 등록 서비스

--- src/main/java/com/knj/mirou/boundedContext/product/controller/ProductController.java
+++ src/main/java/com/knj/mirou/boundedContext/product/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.knj.mirou.boundedContext.product.controller;
 
 import com.knj.mirou.base.rq.Rq;
+import com.knj.mirou.base.rsData.RsData;
+import com.knj.mirou.boundedContext.imageData.model.enums.ImageTarget;
+import com.knj.mirou.boundedContext.imageData.service.ImageDataService;
 import com.knj.mirou.boundedContext.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/product")
@@ -16,6 +21,7 @@ public class ProductController {
 
     private final Rq rq;
     private final ProductService productService;
+    private final ImageDataService imageDataService;
 
     @GetMapping("/add")
     public String addForm() {
@@ -25,15 +31,11 @@ public class ProductController {
 
     @PostMapping("/add")
     public String addProduct(String name, String brandName, int cost, String content, String usingWay,
-                             String usingCaution, MultipartFile img) {
+                             String usingCaution, MultipartFile img) throws IOException {
 
-        System.out.println("name = " + name);
-        System.out.println("brandName = " + brandName);
-        System.out.println("cost = " + cost);
-        System.out.println("content = " + content);
-        System.out.println("usingWay = " + usingWay);
-        System.out.println("usingCaution = " + usingCaution);
-        System.out.println("img.getOriginalFilename() = " + img.getOriginalFilename());
+        String imgUrl = imageDataService.tryUploadImg(img, ImageTarget.PRODUCT_IMG).getData();
+
+        productService.create(name, brandName, cost, content, imgUrl, usingWay, usingCaution);
 
         return rq.redirectWithMsg("/product/add", "등록 되었습니다.");
     }
