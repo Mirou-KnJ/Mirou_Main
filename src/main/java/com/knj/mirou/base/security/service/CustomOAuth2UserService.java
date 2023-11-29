f7f838d868c4ce5f6352505b5b0aefa9784ae465
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-29 10:12:32 +0900

    Remove: 로그인 오류 해결 완료 및 관련 내용 삭제

--- src/main/java/com/knj/mirou/base/security/service/CustomOAuth2UserService.java
+++ src/main/java/com/knj/mirou/base/security/service/CustomOAuth2UserService.java
@@ -32,50 +32,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
-        ClientRegistration clientRegistration = userRequest.getClientRegistration();
-
-        OAuth2AccessToken accessToken = userRequest.getAccessToken();
-
-        Map<String, Object> additionalParameters = userRequest.getAdditionalParameters();
-
-        String registrationId = clientRegistration.getRegistrationId();
-        System.out.println("registrationId = " + registrationId);
-        String clientId = clientRegistration.getClientId();
-        System.out.println("clientId = " + clientId);
-        String clientName = clientRegistration.getClientName();
-        System.out.println("clientName = " + clientName);
-        String clientSecret = clientRegistration.getClientSecret();
-        System.out.println("clientSecret = " + clientSecret);
-        String value = clientRegistration.getClientAuthenticationMethod().getValue();
-        System.out.println("value = " + value);
-        String value1 = clientRegistration.getAuthorizationGrantType().getValue();
-        System.out.println("value1 = " + value1);
-        ClientRegistration.ProviderDetails providerDetails = clientRegistration.getProviderDetails();
-        String authorizationUri = providerDetails.getAuthorizationUri();
-        System.out.println("authorizationUri = " + authorizationUri);
-        String uri = providerDetails.getUserInfoEndpoint().getUri();
-        System.out.println("uri = " + uri);
-        String issuerUri = providerDetails.getIssuerUri();
-        System.out.println("issuerUri = " + issuerUri);
-        String tokenUri = providerDetails.getTokenUri();
-        System.out.println("tokenUri = " + tokenUri);
-        String string = providerDetails.getConfigurationMetadata().toString();
-        System.out.println("string = " + string);
-        String jwkSetUri = providerDetails.getJwkSetUri();
-        System.out.println("jwkSetUri = " + jwkSetUri);
-
-        String value2 = accessToken.getTokenType().getValue();
-        System.out.println("value2 = " + value2);
-        String tokenValue = accessToken.getTokenValue();
-        System.out.println("tokenValue = " + tokenValue);
-        String string1 = accessToken.getScopes().toString();
-        System.out.println("string1 = " + string1);
-        String string2 = accessToken.toString();
-        System.out.println("string2 = " + string2);
-
-        String string3 = additionalParameters.toString();
-        System.out.println("string3 = " + string3);
-
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
         String oauthId = oAuth2User.getName();
