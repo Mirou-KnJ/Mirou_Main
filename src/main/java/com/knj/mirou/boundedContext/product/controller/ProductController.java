cf3a8b4ff3035caa979f7f967f08b9c843211c41
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-30 13:47:59 +0900

    Feat: 물품 코드 등록 성공

--- src/main/java/com/knj/mirou/boundedContext/product/controller/ProductController.java
+++ src/main/java/com/knj/mirou/boundedContext/product/controller/ProductController.java
@@ -1,15 +1,21 @@
 package com.knj.mirou.boundedContext.product.controller;
 
+import com.knj.mirou.base.rsData.RsData;
+import com.knj.mirou.boundedContext.challenge.model.enums.ChallengeLabel;
 import com.knj.mirou.boundedContext.product.model.entity.ProductInfo;
 import com.knj.mirou.boundedContext.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/product")
@@ -25,5 +31,17 @@ public class ProductController {
         model.addAttribute("productInfos", productInfos);
 
         return "view/product/codeAddForm";
+    }
+
+    @ResponseBody
+    @PostMapping("/addCode")
+    public ResponseEntity<RsData<Long>> addCode(@RequestParam Map<String, Object> params) {
+
+        long infoId = Long.parseLong(params.get("infoId").toString());
+        String code = params.get("code").toString();
+
+        RsData<Long> createRs = productService.create(infoId, code);
+
+        return ResponseEntity.ok(createRs);
     }
 }
--- src/main/java/com/knj/mirou/boundedContext/product/service/ProductService.java
+++ src/main/java/com/knj/mirou/boundedContext/product/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.knj.mirou.boundedContext.product.service;
 
+import com.knj.mirou.base.rsData.RsData;
+import com.knj.mirou.boundedContext.product.model.entity.Product;
 import com.knj.mirou.boundedContext.product.model.entity.ProductInfo;
 import com.knj.mirou.boundedContext.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -17,6 +20,19 @@ public class ProductService {
 
     private final ProductInfoService productInfoService;
     private final ProductRepository productRepository;
+
+    public RsData<Long> create(long infoId, String code) {
+
+        Optional<ProductInfo> OInfo = productInfoService.getById(infoId);
+        if(OInfo.isEmpty()) {
+            return RsData.of("F-1", "물품 정보가 유효하지 않습니다.");
+        }
+
+        Product product = new Product(OInfo.get(), code);
+        Product savedProduct = productRepository.save(product);
+
+        return RsData.of("S-1", "코드 등록에 성공하였습니다.", savedProduct.getId());
+    }
 
     public List<ProductInfo> getAllProductInfo() {
 
