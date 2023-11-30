464cf795a92b71a6f9874958cdbd03a85f7351ef
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-30 14:10:24 +0900

    Remove: Store 도메인 삭제

--- src/main/java/com/knj/mirou/boundedContext/store/repository/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.knj.mirou.boundedContext.store.repository;
-
-import com.knj.mirou.boundedContext.store.model.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
