108476b93063ff71fedc9f7bff335f24b24fc8b2
깃SeoMoonK <sunghoon1216@naver.com>
2023-12-02 17:13:56 +0900

    Feat: Inventry MVC 구조 추가

--- src/main/java/com/knj/mirou/boundedContext/inventory/entity/Inventory.java
+++ src/main/java/com/knj/mirou/boundedContext/inventory/entity/Inventory.java
@@ -2,6 +2,7 @@ package com.knj.mirou.boundedContext.inventory.entity;
 
 import com.knj.mirou.base.entity.BaseEntity;
 import com.knj.mirou.boundedContext.member.model.entity.Member;
+import com.knj.mirou.boundedContext.product.model.entity.Product;
 import com.knj.mirou.boundedContext.product.model.entity.ProductInfo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -22,13 +24,11 @@ import java.time.LocalDateTime;
 public class Inventory extends BaseEntity {
 
     @ManyToOne
-    private Member owner;
+    private Member member;
 
     @ManyToOne
-    private ProductInfo productInfo;
+    private Product product;
 
-    private LocalDateTime expDate;
-
-    private String couponNumber;
+    private LocalDate expDate;
 
 }
