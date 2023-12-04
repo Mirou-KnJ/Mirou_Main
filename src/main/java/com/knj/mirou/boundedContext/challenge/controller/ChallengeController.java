a1826a7ccb0fc8768aa68b558af7699f911860c8
jungwoo4503 <108951535+jungwoo4503@users.noreply.github.com>
2023-12-04 11:41:20 +0900

    마이페이지 프론트 수정 (#136)
    Co-authored-by: jeinst <jeinst77@gmail.com>

--- src/main/java/com/knj/mirou/boundedContext/challenge/controller/ChallengeController.java
+++ src/main/java/com/knj/mirou/boundedContext/challenge/controller/ChallengeController.java
@@ -20,8 +20,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
+import java.security.Principal;
+import java.util.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
