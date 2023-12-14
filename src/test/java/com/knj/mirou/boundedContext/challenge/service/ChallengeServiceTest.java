5552428d3e5c56f80e818c11beed95155b603bd8
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-12-14 13:59:29 +0900

    Test: 챌린지 생성시 종료 기한이 적합하지 ã않으면 생성 불가

--- src/test/java/com/knj/mirou/boundedContext/challenge/service/ChallengeServiceTest.java
+++ src/test/java/com/knj/mirou/boundedContext/challenge/service/ChallengeServiceTest.java
@@ -2,6 +2,7 @@ package com.knj.mirou.boundedContext.challenge.service;
 
 import com.knj.mirou.base.rsData.RsData;
 import com.knj.mirou.boundedContext.challenge.model.dtos.ChallengeCreateDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,16 +25,17 @@ public class ChallengeServiceTest {
     private static final String TEST_CHALLENGE_IMG = "https://kr.object.ncloudstorage.com/mirou/etc/system_noti.png";
 
     @Test
-    @DisplayName("t001")
+    @DisplayName("챌린지 생성시 종료 기한이 적합하지 않으면 생성 불가")
     void t001() {
 
-        ChallengeCreateDTO challengeCreateDTO = new ChallengeCreateDTO();
-        challengeCreateDTO.setName("123");
-        challengeCreateDTO.setJoinDeadLine(LocalDate.now().minusDays(1));
+        ChallengeCreateDTO testCreateDTO = new ChallengeCreateDTO();
+        testCreateDTO.setJoinDeadLine(LocalDate.now().minusDays(1));
 
-        RsData<Long> createRs = challengeService.create(challengeCreateDTO, TEST_CHALLENGE_IMG);
+        RsData<Long> createRs = challengeService.create(testCreateDTO, TEST_CHALLENGE_IMG);
 
-        assertThat(createRs.getResultCode()).contains("F");
+        assertThat(createRs.isFail()).isFalse();
     }
+
+
 
 }
