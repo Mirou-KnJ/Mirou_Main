7362b1bc0a094f5b4eb82095c5b8d8d4a881d50b
SeoMoonk <39723465+SeoMoonk@users.noreply.github.com>
2023-12-14 13:46:02 +0900

    테스트 추가시 빌드 성공 여부 테스트 (#190)

--- src/main/java/com/knj/mirou/boundedContext/challenge/model/dtos/ChallengeCreateDTO.java
+++ src/main/java/com/knj/mirou/boundedContext/challenge/model/dtos/ChallengeCreateDTO.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class ChallengeCreateDTO {
 
